7ea07fabce0643698cfb5c9ad093b4eaa82463c2
Divit <divitbatra@hotmail.com>
2023-06-22 19:25:44 +0530

    Dynamic message according to the time of the day

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,12 +1,21 @@
-const http = require('http');
-const today = require('./today');
+const http = require("http");
+const today = require("./today");
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
-}
+  let dateVal = today.getDate();
+  if (dateVal.getHours() < 12) {
+    res.end(`Good morning World! The date today is ${dateVal}`);
+  } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
+    res.end(`Good afternoon World! The date today is ${dateVal}`);
+  } else if (dateVal.getHours() >= 18 && dateVal.getHours() < 21) {
+    res.end(`Good evening World! The date today is ${dateVal}`);
+  } else if (dateVal.getHours() >= 21 && dateVal.getHours() < 24) {
+    res.end(`Good night World! The date today is ${dateVal}`);
+  }
+};
 
 const port = 8080;
 const server = http.createServer(requestListener);
-console.log('server listening on port: ' + port);
+console.log("server listening on port: " + port);
 server.listen(port);
